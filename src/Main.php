add9b23fc9c6d36979a67def4981b0b5452f2b1d
XackiGiFF <m.girsanoff@gmail.com>
2022-12-24 00:10:28 +0300

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -4,18 +4,17 @@ declare(strict_types=1);
 
 namespace XackiGiFF8388\ChunkLimit;
 
-use pocketmine\utils\Config;
-
+use pocketmine\event\Listener;
 use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\network\mcpe\protocol\RequestChunkRadiusPacket;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
 use pocketmine\Server;
+use pocketmine\utils\Config;
 
 class Main extends PluginBase implements Listener{
     private static Main $instance;
-    private static $config;
+    private static $settings;
 
 
     public static function getInstance() : Main {
@@ -26,15 +25,15 @@ class Main extends PluginBase implements Listener{
         self::$instance = $this;
         self::getInstance()->saveResource("config.yml");
         self::$config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-        $this->settings = $this->getConfig()->getAll();
+        self::settings = $this->getConfig()->getAll();
         self::getInstance()->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
     public function onReceive(DataPacketReceiveEvent $event) : void {
-        $chunks = (int) $this->settings['chunks'] ?? 16;
+        $chunks = (int) self::settings['chunks'] ?? 16;
         if (($pk = $event->getPacket()) instanceof RequestChunkRadiusPacket )
             if($pk->radius > $chunks)
-                $pk->radius = (int) $this->settings['chunks'];
+                $pk->radius = (int) self::settings['chunks'];
     }
 
 }
